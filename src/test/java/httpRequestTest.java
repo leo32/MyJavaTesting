4c569bc0d75c2b52fe3d13e5bcf5532799afd548
MSDOMAIN1\wchen3 <wenguang.chen@morningstar.com>
2020-03-21 15:09:15 +0800

    adjust style

--- src/test/java/httpRequestTest.java
+++ src/test/java/httpRequestTest.java
@@ -12,15 +12,22 @@ import java.math.BigDecimal;
 public class httpRequestTest {
     @Test
     public void fetchFile() {
-        String fileUrl = "https://apicenter.morningstar.com/sample/APIJavaSampleCode.zip";
+        String[] serverUrl = {"http://52.82.110.192:8888/","http://52.81.47.5:8888/"};
+        String[] fileName={"1k.zip","10k.zip","100k.zip","500k.zip","1m.zip","10m.zip"};
         TimeInterval timer = DateUtil.timer();
         for (int i = 0; i < 10; i++) {
-            Long fileSize = HttpUtil.downloadFile(fileUrl, FileUtil.file("/Users/wenguang/Downloads"));
-            Long spendTime = timer.interval();
-            BigDecimal speed = NumberUtil.div(spendTime, fileSize);
-            Console.log("文件大小：{},下载时间：{},网速：{}", FileUtil.readableFileSize(fileSize), timer.intervalRestart(), speed);
-            //Console.log(timer.intervalMinute());//花费分钟数
+            for(int j=0;j<serverUrl.length;j++){
+                for(int k=0;k<fileName.length;k++)
+                downloadFile(serverUrl[j]+fileName[k],timer,fileName[k],i);
+            }
         }
 
+    }
+
+    void downloadFile(String fileUrl,TimeInterval timer,String fileName,int i){
+        Long fileSize = HttpUtil.downloadFile(fileUrl, FileUtil.file("./"));
+        Long spendTime = timer.interval();
+        BigDecimal speed = NumberUtil.div(spendTime, fileSize);
+        Console.log("第{}轮测试:文件名：{},文件大小：{},下载时间：{},网速(下载时间/文件大小)：{}",i,fileName, FileUtil.readableFileSize(fileSize), timer.intervalRestart(), speed);
     }
 }
